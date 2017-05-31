ac8ca24a4edf66193a46aeacf59a93ac5f62c95b
Vin <a54959@hotmail.com>
2017-05-31 14:36:00 +0800

    search_api finished

--- spider/richardsonrfpd.com/search_api.py
+++ spider/richardsonrfpd.com/search_api.py
@@ -24,6 +24,7 @@ Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4
 default_headers = util.headers_to_dict(headers_str=headers_string)
 print default_headers
 
+
 def get_event_kwargs(response=None, **kwargs):
     """获取网页中隐藏表单的值
     返回整理好的触发事件需要的表单数据字典
@@ -62,6 +63,7 @@ def do_search(response=None, keyword=''):
         return -400
     form_data = {}
     event_data = get_event_kwargs(response)
+    sess = requests.session()
     # 搜索事件 表单构造
     form_data['ctl00$SPWebPartManager1$g_096ee875_b50a_46d8_828c_52a9076a775d$txtPartNumber'] = keyword
     form_data['ctl00$SPWebPartManager1$g_096ee875_b50a_46d8_828c_52a9076a775d$ctl03'] = 'Starts with'
@@ -71,42 +73,59 @@ def do_search(response=None, keyword=''):
     _headers = copy.copy(default_headers)
     _headers.update({'Content-Type': 'application/x-www-form-urlencoded'})
     try:
-        result_response = requests.post(url=response.url, headers=_headers, data=form_data)
+        result_response = sess.post(url=response.url, headers=_headers, data=form_data)
         with open(r'html/search_result_no_detail.html', 'w') as fp:
             fp.write(result_response.text.encode('utf-8'))
     except:
         return -400
-    # 显示所有搜索接口
+    # 显示所有搜索
     if 'Search-Results.aspx' in result_response.url:
+        root = lxml.html.fromstring(result_response.text.encode('utf-8'))
+        product_table = root.xpath('//table[@class="PSTable"]')
+        if product_table:
+            return result_response
         form_data = {'__EVENTTARGET': 'ctl00$SPWebPartManager1$g_539e9434_3497_4d6a_badc_24c5f583680c$btnBottomShowAll'}
         event_data = get_event_kwargs(result_response)
         form_data.update(event_data)
         _headers.update({'Referer': result_response.url})
         try:
-            resp = requests.post(url=result_response.url, headers=_headers, data=form_data, allow_redirects=False)
+            resp = sess.post(url=result_response.url, headers=_headers, data=form_data, allow_redirects=False)
+            return resp
         except:
             return -400
-    return resp
+    elif 'Product-Details' in result_response.url:
+        return result_response
 
 
 def main():
     form_data = {}
-    sess = requests.session()
+    # sess = requests.session()
     url = 'http://www.richardsonrfpd.com/Pages/AdvanceSearch.aspx'
     # url = 'http://www.richardsonrfpd.com/Pages/Product-End-Category.aspx?productCategory=10047'
-    response = sess.get(url=url, data=form_data, headers=default_headers)
+    # response = sess.get(url=url, data=form_data, headers=default_headers)
     #
     # print response.status_code
     # with open(r'html/AdvanceSearch.html', 'w') as fp:
     #     fp.write(response.text.encode('utf-8'))
     #
     # time.sleep(1)
-    # with open(r'html/home.html', 'r') as fp:
-    #     html = fp.read()
-    resp = do_search(response, 'ANT')
+    with open(r'html/search_result_show_all.html', 'r') as fp:
+        html = fp.read()
+    # resp = do_search(response, 'ANT')
+    # product_list
+    root = lxml.html.fromstring(html)
+    product_id = root.xpath('//table[@class="PSTable"]/@id')
+    product_id = product_id[0] if product_id else None
+    product_list = root.xpath('//a[starts-with(@id, {pid})]'.format(pid=repr(product_id)))
+    print product_list[0].text.strip()
+    product_list = root.xpath('//a[starts-with(@id, {pid})]/@href'.format(pid=repr(product_id)))
+    print product_list
+    base_url = 'http://www.richardsonrfpd.com/Pages/Search-Results.aspx'
+    product_list = [urlparse.urljoin(base_url, x) for x in product_list]
+    print product_list
+    # with open(r'html/search_result_show_all.html', 'w') as fp:
+    #     fp.write(resp.text.encode('utf-8'))
 
-    with open(r'html/search_result_show_all.html', 'w') as fp:
-        fp.write(resp.text.encode('utf-8'))
 
 if __name__ == '__main__':
     main()
